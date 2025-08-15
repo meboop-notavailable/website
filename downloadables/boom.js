ff708336ad9d227d5608065f8c2585c841a0d7b1
Meboop!! <mvpgens.mc@gmail.com>
2025-08-14 19:05:16 -0500

    Update boom.js

--- downloadables/boom.js
+++ downloadables/boom.js
@@ -1,1 +1,9 @@
-
+javascript:(function() {
+function se(d) {
+    return d.selection ? d.selection.createRange().text : d.getSelection()
+} 
+var s = se(document); 
+for (var i=0; i<frames.length && (s==null || s==''); i++) s = se(frames[i].document); 
+if (!s || s=='') s = prompt('Enter%20search%20terms%20for%20Wikipedia',''); 
+open('https://en.wikipedia.org' + (s ? '/w/index.php?title=Special:Search&search=' + encodeURIComponent(s) : '')).focus();
+})();
